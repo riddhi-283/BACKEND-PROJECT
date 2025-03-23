6a353964258f56c252b51cf67a38703f8a9373ee
riddhi-283 <lci2022040@iiitl.ac.in>
2025-03-23 18:33:07 +0530

    add getwatchHistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { apiResponse } from "../utils/apiResponse.js"
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
 
 const generateAccessandRefreshTokens = async(userId) => {
     try {
@@ -423,6 +424,54 @@ const getUserChannelProfile = asyncHandler(async(req,res) => {
   .json(new apiResponse(200,channel[0], "User channel fetched successfully!"))
 })
 
+const getWatchHistory = asyncHandler(async(req,res) => {
+   const user = await User.aggregate([
+    {
+        $match:{
+            _id:new mongoose.Types.ObjectId(req.user._id)
+        }
+    },
+    {
+        $lookup:{
+            from:"videos",
+            localField:"watchHistory",
+            foreignField:"_id",
+            as:"watchHistory",
+            pipeline:[
+                {
+                    $lookup:{
+                        from:"users",
+                        localField:"owner",
+                        foreignField:"_id",
+                        as:"owner",
+                        pipeline:[
+                            {
+                                $project:{
+                                    fullname:1,
+                                    username:1,
+                                    avatar:1
+                                }
+                            }
+                        ]
+                    }
+                },
+                {
+                    $addFields:{
+                        owner:{
+                            $first:"$owner"
+                        }
+                    }
+                }
+            ]
+        }
+        // now we have docs of all videos in watchhistory, but we have to run a nested loop to get ownder details of watched videos
+
+    }
+   ])
+
+   return res.status(200)
+   .json(new apiResponse(200,user[0].watchHistory,"Watch history fetched successfully!"))
+})
 
 export {
     registerUser,
@@ -433,5 +482,7 @@ export {
     getCurrentUser, 
     updateAccountDetails, 
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 }
